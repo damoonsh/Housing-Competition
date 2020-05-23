c46dcdadd0a5de8211c5a777ba366ba6c7125875
Damoon <d.shahhosseini@yahoo.com>
2020-05-23 10:20:31 -0400

    Changed the structure

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -71,3 +71,24 @@ def tryOut(features, n, features_in=[]):
         index += 1
         
     return pd.DataFrame(r2s)
+
+"""
+    Adding polynomials to the features
+"""
+def polynomial_options(df, features, y_feature, threshhold=0.011):
+
+    for feature in features:
+        df[feature + '^2'] = df[feature] ** 2
+        df[feature + '^3'] = df[feature] ** 3
+        df[feature + '^4'] = df[feature] ** 4
+
+        relations = df.corr()[y_feature]
+        base = relations[feature]
+
+        if not (relations[feature + '^4'] - base > 0.11 or relations[feature + '^4'] - relations[feature + '^3'] > threshhold  or relations[feature + '^4'] - relations[feature + '^2'] > threshhold):
+            df.drop(feature + '^4', axis=1)
+        if not (relations[feature + '^3'] - base > 0.11 or relations[feature + '^3'] - relations[feature + '^2'] > threshhold):
+            df.drop(feature + '^3', axis=1)
+        if not (relations[feature + '^2'] - base > threshhold):
+            df.drop(feature + '^2', axis=1)
+    return df
--- House.py
+++ House.py
@@ -1,12 +1,12 @@
 from utils import *
 
-
-""" 
-    Setting up the initial values 
-"""
+""" Setting up the initial values """
 # The value that we are to predict
 y_feature = 'SalePrice'
 
 # Pass no parameters since it will fetch the training data
 train = load_house_data()
-test = load_house_data("test.csv")
+# Pass the file name for the test.csv file
+test = load_house_data(fileName="test.csv")
+
+
