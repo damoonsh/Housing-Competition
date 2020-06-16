faa5e9df17056c74df558abd26b4d106c4946727
Damoon <d.shahhosseini@yahoo.com>
2020-06-16 14:46:31 -0400

    Minor changes

--- utils.py
+++ utils.py
@@ -17,9 +17,9 @@ def load_data(fileName="train.csv", base_path="./dataset/"):
     return pd.read_csv(path)
 
 
-def load_bench_data():
+def load_bench_data(file_name, root='./submissions/'):
     """ Loads and formats the benchmark data """
-    bench = load_data('bench-mark.csv' ,'./dataset/')
+    bench = load_data(file_name ,root)
     bench['Id'] = bench['Id'] - bench.shape[0] - 1 
     bench = bench.set_index('Id')
 
@@ -96,7 +96,7 @@ def quantize(values):
         # Returns: the modified version
     """
     modified = [] 
-    for num in list(pd.DataFrame(pred_y)[0].values):
+    for num in list(values):
         if num - int(num) >= 0.5:
             modified.append(int(num) + 1)
         else:
@@ -391,80 +391,3 @@ def normalize(col, Type='std'):
         return (col - col.mean()) / col.mean()
     
     return (col - col.mean()) / col.std()
-
-
-def build_model01():
-  model = keras.Sequential([
-    layers.Dense(64, input_shape=[len(std_d.keys())]),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(128, activation='softmax'),
-    layers.Dropout(0.5),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(256, activation='softmax'),
-    layers.Dropout(0.5),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(128, activation='softmax'),
-    layers.Dropout(0.5),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(1)
-  ])
-
-  optimizer = tf.keras.optimizers.RMSprop(0.01)
-
-  model.compile(loss='mse',
-                optimizer=optimizer,
-                metrics=['mse', 'msle'])
-  return model
-
-def build_model02():
-  model = keras.Sequential([
-    layers.InputLayer(input_shape=[len(X.keys())]),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(64),
-    layers.Dense(128),
-    layers.Dense(256),
-    layers.BatchNormalization(),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(256),
-    layers.Dense(512),
-    layers.Dense(1024),
-    layers.Dropout(0.3),
-    layers.BatchNormalization(),
-    layers.Dense(512, activation='relu'),
-    layers.Dense(512),
-    layers.Dense(256),
-    layers.Dense(128),
-    layers.BatchNormalization(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(64),
-    layers.Dense(32),
-    layers.Dense(16),
-    layers.Dense(1)
-  ])
-
-  optimizer = tf.keras.optimizers.Adam(0.001)
-
-  model.compile(loss='msle',
-                optimizer=optimizer,
-                metrics=['msle']
-               )
-  return model
-# 0.16:
-def build_model03():
-  model = keras.Sequential([
-    layers.InputLayer(input_shape=[len(X.keys())]),
-    layers.BatchNormalization(),
-    layers.Dense(64),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(64),
-    layers.Dense(32, activation='relu'),
-    layers.Dense(1)
-  ])
-
-  optimizer = tf.keras.optimizers.Adam(0.001)
-
-  model.compile(loss='msle',
-                optimizer=optimizer,
-               )
-  return model
