2a6eca9f98d950ee18e2fdb856ac8dcc4b69d084
Damoon <d.shahhosseini@yahoo.com>
2020-06-26 18:47:11 -0400

    Fixed dictation errors

--- models.py
+++ models.py
@@ -1,20 +1,16 @@
-# Each of the models should return a sries of values that is going to be used to train them
-# sicne these values have been previousley determined by testings.
-# props : {learning_rate: int, optimizer: Optimizer, }
-
 """
-	Some constant utilites that all the models will be using:
+	Some constant utilities that all the models will be using:
 	1. The evaluation metric used for the competition is Mean Squared Logarithmic Error 
 		hence all the models are going to be using the MSLE as their evaluation metric.
 	2. The Default optimizer will be Adam and RSMprop, also we could choose to use a 
 		learning_rate modifier for the Optimizer which would either be a InverseTimeDecay 
 		or ExponentialDecay.
-	3. They are only few type of layers and activation that work reasonbly for Regression problems:
+	3. They are only few type of layers and activation that work reasonably for Regression problems:
 			Layers: Dense, Dropout, BatchNormalization
 			activations: relu, elu, tanh*
 	4. For each model, there should be a config dictionary that descrives values that best work for
-		the model and this values should have been drived by testing and parameter tuning (potentially
-		using keras tuner).
+		the model and this values should have been driven by testing and parameter tuning (potentially
+		using Keras-tuner).
 	5. Each function that implements the model gets a full configuration on both compilation and 
 		fitting parameters that the model going to need.
 	6. The monitored parameter in a EarlyStopping object is always 'val_loss', since the assumption
@@ -23,7 +19,7 @@
 	Note01: BatchNormalization layers should be used only at the beginning layer of the model since
 		it does not work when used in different parts of the model.
 	Note02: tanh does not happen to work as well as elu and relu, on a broader perspective relu is 
-		most appropiate one for the regression.
+		most appropriate one for the regression.
 """
 
 
@@ -31,15 +27,7 @@
 from tensorflow.keras.optimizers.schedules import InverseTimeDecay, ExponentialDecay
 
 def TimeDecayScheduler(learning_rate=0.001, decay_steps=200, decay_rate=1.2, name=""):
-	""" 
-		Returns an InverseTimeDecay object with the given properties to be used in the optimizer.
-		
-		# params: 
-			all the parametes will be needing for a non-staircase InverseTimeDecay Scheduleer
-			
-		# returns: 
-			a InverseTimeDecay object to monitize our optimizer's learning rate
-	"""
+	""" Returns an InverseTimeDecay object with the given properties to be used in the optimizer. """
 	return InverseTimeDecay(
 		initial_learning_rate=learning_rate, 
 		decay_steps=decay_steps,
@@ -48,19 +36,10 @@ def TimeDecayScheduler(learning_rate=0.001, decay_steps=200, decay_rate=1.2, nam
     )
 
 
-def ExonentialScheduler(initial_learning_rate, decay_steps, decay_rate, name=""):
-	"""
-		Returns an ExponentialDecay object with the given properties to be used in the optimizer.
-		
-		# params: 
-			all the parametes will be needing for a non-staircase ExponentialDecay Scheduleer
-			
-		# returns: 
-			a InverseTimeDecay object to monitize our optimizer's learning rate
-	
-	"""
+def ExponentialScheduler(initial_learning_rate, decay_steps, decay_rate, name=""):
+	""" Returns an ExponentialDecay object with the given properties to be used in the optimizer. """
 	return InverseTimeDecay(
-		initial_learning_rate=learning_rate, 
+		initial_learning_rate=initial_learning_rate, 
 		decay_steps=decay_steps,
 		decay_rate=decay_rate,
 		name=name
@@ -75,7 +54,7 @@ def AdamOptimizer(learning_rate=0.001, scheduler=None):
 	"""
 		# params:
 			learning_rate: the initial learning rate to be used
-			scheduler: If this is passed by the user then use it in the optimizer isntead of the learning rate
+			scheduler: If this is passed by the user then use it in the optimizer instead of the learning rate
 		
 		# returns: an Adam optimizer
 	"""
@@ -90,7 +69,7 @@ def RMSpropOptimizer(learning_rate=0.001, scheduler=None):
 		# params:
 			learning_rate: the initial learning rate to be used
 			scheduler: If this is passed by the user then use it in the 
-						optimizer isntead of the learning rate
+						optimizer instead of the learning rate
 		
 		# returns: an RMSprop optimizer
 	"""
@@ -112,14 +91,14 @@ def EarlyStopCallBack(patience=100):
 	
 # Models: 
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization  # Layers 
-from tensorflow.keras.regularizers import l2, l1, l1_l2, L1L2  # Regularizers
+from tensorflow.keras.regularizers import l2, l1, l1_l2, L1L2  # Regularizer
 from tensorflow.keras.losses import MeanSquaredLogarithmicError # Error-metric
 import tensorflow_docs as tfdocs # For logging puposes
 
 def Model01(config):
 	"""
 		# params: 
-			config: uses the condifuration dictionary to compile and fit the model accordingly
+			config: uses the configuration dictionary to compile and fit the model accordingly
 			
 		# returns a history object when the fitting is done
 	"""
